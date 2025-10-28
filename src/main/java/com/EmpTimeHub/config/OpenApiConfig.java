86acead9fe5d2557f67db4ec546d56bf9429ae61
dq-aem-shoaib <shoaibm@digiquadsolutions.com>
2025-10-28 16:49:34 +0530

    Add Server import to OpenApiConfig

--- src/main/java/com/EmpTimeHub/config/OpenApiConfig.java
+++ src/main/java/com/EmpTimeHub/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
